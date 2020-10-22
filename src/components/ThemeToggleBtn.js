c9d36eb009f07151a955b968b9c064435b2b3d1b
Franco <francopanozzo@hotmail.com.ar>
2020-10-22 21:12:22 +0200

    Add toggle themes button functionality

--- src/components/ThemeToggleBtn.js
+++ src/components/ThemeToggleBtn.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function ThemeToggleBtn() {
-  const theme = 'dark';
+  const [theme, setTheme] = useState('dark');
+
+  useEffect(() => {
+    const globalStyles = document.documentElement.style;
+    if (theme !== 'light') {
+      // Set to light
+      globalStyles.setProperty('--main-bg', 'hsl(230, 17%, 14%)');
+      globalStyles.setProperty('--top-bg', 'hsl(232, 19%, 15%)');
+      globalStyles.setProperty('--card-bg', 'hsl(228, 28%, 20%)');
+      globalStyles.setProperty('--muted-text', 'hsl(228, 34%, 66%)');
+      globalStyles.setProperty('--regular-text', 'hsl(0, 0%, 100%)');
+    } else {
+      // Set to dark
+      globalStyles.setProperty('--main-bg', ' hsl(0, 0%, 100%)');
+      globalStyles.setProperty('--top-bg', 'hsl(225, 100%, 98%)');
+      globalStyles.setProperty('--card-bg', 'hsl(227, 47%, 96%)');
+      globalStyles.setProperty('--muted-text', 'hsl(228, 12%, 44%)');
+      globalStyles.setProperty('--regular-text', 'hsl(230, 17%, 14%)');
+    }
+  }, [theme]);
+
+  const toggleThemes = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
   return (
-    <button className={`themeToggleBtn ${theme}`}>
+    <button className={`themeToggleBtn ${theme}`} onClick={toggleThemes}>
       <span>dark mode</span>
       <div className="pill">
         <div className="circle"></div>
