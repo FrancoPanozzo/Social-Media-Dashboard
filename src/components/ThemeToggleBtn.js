7f714e7c66fc8ccade67eac11989beb3b29811af
Franco <francopanozzo@hotmail.com.ar>
2020-10-26 09:32:52 +0100

    Add hover effects to the cards with theme functionality in CSS variables

--- src/components/ThemeToggleBtn.js
+++ src/components/ThemeToggleBtn.js
@@ -5,18 +5,20 @@ export default function ThemeToggleBtn() {
 
   useEffect(() => {
     const globalStyles = document.documentElement.style;
-    if (theme !== 'light') {
-      // Set to light
+    if (theme === 'dark') {
+      // Set to dark
       globalStyles.setProperty('--main-bg', 'hsl(230, 17%, 14%)');
       globalStyles.setProperty('--top-bg', 'hsl(232, 19%, 15%)');
       globalStyles.setProperty('--card-bg', 'hsl(228, 28%, 20%)');
+      globalStyles.setProperty('--card-hover-bg', 'hsl(228, 26%, 27%)');
       globalStyles.setProperty('--muted-text', 'hsl(228, 34%, 66%)');
       globalStyles.setProperty('--regular-text', 'hsl(0, 0%, 100%)');
     } else {
-      // Set to dark
+      // Set to light
       globalStyles.setProperty('--main-bg', ' hsl(0, 0%, 100%)');
       globalStyles.setProperty('--top-bg', 'hsl(225, 100%, 98%)');
       globalStyles.setProperty('--card-bg', 'hsl(227, 47%, 96%)');
+      globalStyles.setProperty('--card-hover-bg', 'hsl(232, 33%, 91%)');
       globalStyles.setProperty('--muted-text', 'hsl(228, 12%, 44%)');
       globalStyles.setProperty('--regular-text', 'hsl(230, 17%, 14%)');
     }
